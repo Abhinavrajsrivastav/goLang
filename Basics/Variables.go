112069bc5bb3d8241316db9732501f6e5a473968
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2025-02-13 22:43:20 +0530

    13-01-2025-001

---
+++ Basics/Variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Variables
+    var name string = "John"
+    var age int = 30
+    fmt.Println("Name:", name, "Age:", age)
+
+    // Constants
+    const pi = 3.14
+    fmt.Println("Value of Pi:", pi)
+}
