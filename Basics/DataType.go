4f069a5d292077c14b70850210f70fed350e4941
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2025-02-14 22:22:44 +0530

    14-01-2025-001

---
+++ Basics/DataType.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x int = 10
+    var y float64 = 3.14
+    var message string = "Golang"
+    var isTrue bool = true
+
+    fmt.Println(x, y, message, isTrue)
+}
