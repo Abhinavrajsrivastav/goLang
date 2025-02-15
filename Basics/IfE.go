b1f5794b348172070c2fa6165e91f5f435fd2e36
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2025-02-15 21:26:40 +0530

    15-01-2025-001

---
+++ Basics/IfE.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    x := 20
+    if x > 10 {
+        fmt.Println("x is greater than 10")
+    } else {
+        fmt.Println("x is less than or equal to 10")
+    }
+}
